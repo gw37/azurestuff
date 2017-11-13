cc811fdc5019edf335427288313f7cb8f4791873
glenn walton <glenn@cco.com>
2017-11-13 11:03:18 -0800

    Delete joindom-dogs.ps1

--- joindom-dogs.ps1
+++
@@ -1,5 +0,0 @@
-$secpasswd = ConvertTo-SecureString "Pass@word1" -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ("dogs\joiner", $secpasswd)
-mkdir c:\poshlog -ErrorAction Ignore
-add-computer -computername $env:computername -domainname dogs.local  -Credential $cred -restart –force >c:\poshlog\domjoin.txt
-
