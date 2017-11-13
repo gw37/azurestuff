d4584239da36e14c48836994330b44847d501107
glenn walton <glenn@cco.com>
2017-11-13 11:07:01 -0800

    Add files via upload

---
+++ joindog2.ps1
@@ -0,0 +1,4 @@
+﻿$secpasswd = ConvertTo-SecureString "Pass@word1" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ("dogs\joiner", $secpasswd)
+mkdir c:\poshlog -ErrorAction Ignore
+add-computer -ComputerName $env:COMPUTERNAME -DomainName dogs.local -Credential $cred -restart -force > c:\poshlog\joindog2.txt
