0422988b44a06e41ec819387be29c2c98a2d2a14
glenn walton <glenn@cco.com>
2017-11-13 11:01:21 -0800

    Add files via upload

--- joindom-dogs.ps1
+++ joindom-dogs.ps1
@@ -1,5 +1,5 @@
 $secpasswd = ConvertTo-SecureString "Pass@word1" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("dogs\joiner", $secpasswd)
 mkdir c:\poshlog -ErrorAction Ignore
-add-computer -computername $env:computername -domainname dogs.local –credential $cred -restart –force >c:\poshlog\domjoin.txt
+add-computer -computername $env:computername -domainname dogs.local  -Credential $cred -restart –force >c:\poshlog\domjoin.txt
 
