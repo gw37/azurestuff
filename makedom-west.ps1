46fed9964922000f8be553d7f77becf40dcd80d9
glenn walton <glenn@cco.com>
2017-11-14 19:58:02 -0800

    Add files via upload

--- makedom-west.ps1
+++ makedom-west.ps1
@@ -12,8 +12,5 @@ $safemodepassword = ConvertTo-SecureString -String "safe-m0dePass"  -AsPlainText
 $domainname = "ccowest.local" 
 $netbiosName = "CCOwest" 
 Import-Module ADDSDeployment 
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" `
--DomainMode "Win2012" -DomainName $domainname  `
--DomainNetbiosName $netbiosName -ForestMode "Win2012"  `
--InstallDns:$true  -LogPath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL"  `
--NoRebootOnCompletion:$false   -SafeModeAdministratorPassword $safemodepassword  -Force:$true > c:\poshlog\newADforest.txt
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012" -DomainName $domainname  -DomainNetbiosName $netbiosName -ForestMode "Win2012"  `
+-InstallDns:$true  -LogPath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL"  -NoRebootOnCompletion:$false   -SafeModeAdministratorPassword $safemodepassword  -Force:$true > c:\poshlog\newADforest.txt
